d2e23c717310231de1dfc991d12aed3aeb7d2584
azizrahimov2509 <rahimov2509@mail.com>
2024-05-26 22:24:36 +0300

    Redux darkmode

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+
+function App() {
+  const countValue = useSelector((state) => state?.count);
+  const mode = useSelector((state) => state?.darkmode);
+
+  const dispatch = useDispatch();
+
+  const toggleDarkMode = () => {
+    dispatch({ type: "CHANGE_MODE" });
+  };
+
+  useEffect(() => {
+    if (mode) {
+      document.body.classList.add("darkmode");
+    } else {
+      document.body.classList.remove("darkmode");
+    }
+  }, [mode]);
+
+  return (
+    <>
+      <div>
+        <div className="switchMode">
+          <button onClick={toggleDarkMode}>Darkmode</button>
+        </div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => dispatch({ type: "INCREMENT" })}>
+          count is {countValue ?? 0}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
